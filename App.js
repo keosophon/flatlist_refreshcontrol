49cb9b4cc82aa098c9dbea17b76b6c400c7e6f2d
keosophon <keosophon8888@gmail.com>
2024-09-09 12:36:02 +1200

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, FlatList, Text, StyleSheet, RefreshControl } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  const [data, setData] = useState([{ id: "1", title: "Item 1" }]);
+
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = () => {
+    setRefreshing(true);
+
+    setTimeout(() => {
+      // Optionally update data
+      setData((prevData) => [
+        ...prevData,
+        {
+          id: (prevData.length + 1).toString(),
+          title: `Item ${prevData.length + 1}`,
+        },
+      ]);
+      setRefreshing(false);
+    }, 1000);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={styles.item}>
+              <Text style={styles.text}>{item.title}</Text>
+            </View>
+          )}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+              colors={["red"]}
+            />
+          }
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    flex: 1,
+    marginHorizontal: 16,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    fontStyle: "italic",
+    textAlign: "center",
   },
 });
